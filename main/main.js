496503728fb85951f882af7db2c3e627636fd770
Luis Zambrano <zambranoluis383@gmail.com>
2022-05-16 15:26:45 -0400

    actualizacion

--- main/main.js
+++ main/main.js
@@ -1,21 +1,23 @@
-const Api_Tranding_Url = 'https://api.themoviedb.org/3/trending/movie/day?api_key=e9f1a36d7b4f761f8a4fbf4fe67eb64f'
+const Api_Tranding_Movie_Url = 'https://api.themoviedb.org/3/trending/movie/day?api_key=e9f1a36d7b4f761f8a4fbf4fe67eb64f'
+const Api_Tranding_Tv_Url = 'https://api.themoviedb.org/3/trending/tv/day?api_key=e9f1a36d7b4f761f8a4fbf4fe67eb64f'
 const Img_Url ='https://image.tmdb.org/t/p/w500/';
+
+
 async function trandingMovies (){
-    const res = await fetch(Api_Tranding_Url);
+    const res = await fetch(Api_Tranding_Movie_Url);
     const data = await res.json();
     const results = data.results;
 
-    
     const err = document.getElementById("error");
-    
-    
-    
+        
     if(res.status !== 200){
         err.innerHTML = `Error: ${res.status} ${data.status_message}`
     } else {
         results.forEach(element => {
             const posterPath = element.poster_path;
             const MovieTitle = element.original_title;
+            const id = element.id
+
             const trandingContainer = document.getElementById('most-pupulra-container');
             const div = document.createElement('div');
             const aImg = document.createElement('a');
@@ -25,7 +27,8 @@ async function trandingMovies (){
 
             
             div.className = 'movie-popular-container';
-            
+            div.id = id;
+
             aImg.href = './movie.html';
 
             img.src = Img_Url + posterPath;
@@ -46,7 +49,30 @@ async function trandingMovies (){
         });
         console.log(data);
     }
-   
 }
 
 trandingMovies();
+
+async function trandingTv(){
+    const res = await fetch(Api_Tranding_Tv_Url);
+    const data = await res.json();
+    const tvData = data.results;
+
+    tvData.forEach(element => {
+        const urlPoster = element.poster_path;
+        const container = document.getElementById('test');
+
+        const div = document.createElement('div')
+        div.className = 'movie-uncoming-container'
+        const img = document.createElement('img');
+        img.className = 'movie-uncoming-img'
+        img.src = Img_Url + urlPoster;
+
+        div.appendChild(img)
+
+        container.appendChild(div)
+
+    });
+}
+
+trandingTv();
